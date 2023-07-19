0d15fce6df969b526aee97a0f7baa2b858796b5e
Gajanand9608 <gajanandsharmajsr@gmail.com>
2023-07-19 22:12:49 +0530

    basic styling done

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async ()=>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random() *1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp =new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title:`${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/2184453',
+            description:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita perferendis animi maiores dolorem natus, totam fuga quod rerum, sapiente voluptates quos facere culpa illum fugiat nemo suscipit illo autem repellendus.",
+            price
         })
         await camp.save();
     }
