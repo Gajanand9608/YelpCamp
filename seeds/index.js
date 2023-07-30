b369a321d81683ca7f014608df5a95e284eecf3f
Gajanand9608 <gajanandsharmajsr@gmail.com>
2023-07-30 15:57:17 +0530

    authorization done -- refatorization next

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async ()=>{
         const random1000 = Math.floor(Math.random() *1000);
         const price = Math.floor(Math.random()*20)+10;
         const camp =new Campground({
+            author: '64c5f96bef4d8519b78acd7e',
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title:`${sample(descriptors)} ${sample(places)}`,
             image:'https://source.unsplash.com/collection/2184453',
