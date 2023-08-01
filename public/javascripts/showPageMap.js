dfb91c85c51daf40780292300145e98f6514363e
Gajanand9608 <gajanandsharmajsr@gmail.com>
2023-08-01 14:40:16 +0530

    final styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,6 +7,9 @@ const map = new mapboxgl.Map({
     zoom: 12, // starting zoom
 });
 
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({offset:25}).setHTML(`<h3>${campground.title}</h3> <p>${campground.location}</P>`))
